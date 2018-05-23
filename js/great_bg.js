3729df47d412573e171630325b55b0c6d2975cd2
shanemmay <shanemarvinmay@hotmail.com>
2018-05-23 13:43:50 -0500

    made the exterior css sheet. Also made it so it starts from blue to red because it looks the best.

--- js/great_bg.js
+++ js/great_bg.js
@@ -1,16 +1,32 @@
-var c1 = document.querySelector('#color1');
-var c2 = document.querySelector('#color2');
+var color1 = document.querySelector('#color1');
+var color2 = document.querySelector('#color2');
 var css = document.querySelector("h3");
 var body = document.querySelector("body");
+var btn = document.querySelector("#btn");
+
 
 function set_gradient(n)
 {
-body.style.background = "linear-gradient(to right," + color1.value + ", " + color2.value + ")";	
-console.log(n);
+	body.style.background = "linear-gradient(to right," + color1.value + ", " + color2.value + ")";	
 
-css.textContent = body.style.background + ";";
+	console.log(n);
+
+	css.textContent = body.style.background + ";";
+}
+
+function random_color(n)
+{
+	console.log(n);
+
+	color1.value = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	color2.value =  '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+
+	set_gradient(n);
+
 }
 
 color1.addEventListener("input", set_gradient);
 
 color2.addEventListener("input", set_gradient);
+
+btn.addEventListener( "click", random_color);
